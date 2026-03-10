f130c802dd62b241539ad4a060f4afeb4d3026e9
David Basilio de Melo <davidbasilio2108@gmail.com>
2026-03-10 15:21:58 -0300

    Adição de painel de Login, Registro, Otimização de código e resolulçao de bugs relacionados a saque e deposito.

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,7 +2,56 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        int log = 0;
+        String entradaUsuario, entradaSenha;
+        String name;
+        String senha;
+        Scanner scan = new Scanner(System.in);
+
+
+                System.out.println("-----Painel de Login-----");
+                System.out.println("     Registrar conta     ");
+                System.out.print("Usuário: ");
+                name = scan.nextLine();
+                System.out.print("Senha: ");
+                senha = scan.nextLine();
+                System.out.println("Registro Feito com sucesso!");
+                System.out.println("Iniciando Login...");
+                for (int i = 0; i <= 10; i++) {
+                    System.out.print("■");
+
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        for (int i = 0; i < 50; i++) {
+            System.out.println("                ");
+        }
+
+                do {
+                    System.out.println(" ");
+                    System.out.println("-----Painel de Login-----");
+                    System.out.print("| Digite seu usuário: ");
+                    entradaUsuario = scan.nextLine();
+                    System.out.print("| Digite sua Senha: ");
+                    entradaSenha = scan.nextLine();
+                    if (entradaUsuario.equals(name) && entradaSenha.equals(senha)) {
+                        System.out.println("Usuario e senha corretos!");
+                        System.out.println("Seja Bem Vindo!");
+                        log = 1;
+                    } else {
+                        System.out.println("Usuário ou Senha Incorreto");
+                        System.out.println("Sistema Fechando...");
+                        log = 0;
+                    }
+
+
+                } while (log != 1);
+
+
         int escolha;
         double saldo = 0, ver;
         do {
@@ -13,8 +62,7 @@ public class Main {
             System.out.println("|   [4] Sair               |");
             System.out.println("----------------------------");
             System.out.print("Escolha: ");
-            Scanner sc = new Scanner(System.in);
-            escolha = sc.nextInt();
+            escolha = scan.nextInt();
             for (int i = 0; i < 50; i++) {
                 System.out.println("                ");
             }
@@ -22,14 +70,18 @@ public class Main {
                 case 1:
                     System.out.println("-----Painel do Bancário-----");
                     System.out.print(" Sacar: R$");
-                    ver = sc.nextInt();
-                    saldo = saldo - ver ;
+                    ver = scan.nextDouble();
+                    if (saldo < ver){
+                        System.out.println("Você não tem dinheiro suficiente.");
+                    } else {
+                        saldo = saldo - ver ;
+                    }
                     break;
                 case 2:
                     System.out.println("-----Painel do Bancário-----");
                     System.out.print("Deposito: R$");
-                    ver = sc.nextInt();
-                    saldo = ver++;
+                    ver = scan.nextDouble();
+                    saldo += ver;
                     break;
                 case 3:
                     System.out.println("-----Painel do Bancário-----");
